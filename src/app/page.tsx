187b01097cdeb742bfbe72258922597ac13f7d8f
Rychillie <rychillie@rychillie.net>
2024-12-06 17:57:01 -0300

    Fix: Format and Linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ export default function Home() {
     <div className="flex w-full flex-col gap-6 overflow-scroll p-6">
       <div className="flex items-center justify-between rounded-xl border border-transparents-400 p-4">
         <div className="flex items-center justify-center gap-3">
-          <Input icon='search' placeholder="Search..." />
-          <Button icon='filter' intent="secondary">Add filter</Button>
+          <Input icon="search" placeholder="Search..." />
+          <Button icon="filter" intent="secondary">
+            Add filter
+          </Button>
         </div>
 
         <div className="flex items-center justify-center gap-3">
-          <Button icon='chevron-down' iconPosition='right' intent="secondary">5 entities</Button>
+          <Button icon="chevron-down" iconPosition="right" intent="secondary">
+            5 entities
+          </Button>
           <Button>Create Invoice</Button>
         </div>
       </div>
